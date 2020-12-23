36315984d66e85df5207abe6bfbfd4e00b4c8616
EOstling <it.treugott@gmail.com>
2020-12-23 17:02:07 -0500

    updated boilerplate

---
+++ src/system.cpp
@@ -0,0 +1,56 @@
+#include <unistd.h>
+#include <cstddef>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "process.h"
+#include "processor.h"
+#include "system.h"
+
+using std::set;
+using std::size_t;
+using std::string;
+using std::vector;
+
+
+// TODO: Might need to better define each of these subroutines
+Processor& System::Cpu() { 
+    return cpu_;
+}
+
+vector<Process>& System::Processes() { 
+    return processes_;
+    
+ }
+
+std::string System::Kernel() { 
+    return string();
+
+}
+
+float System::MemoryUtilization() { 
+    return 0.0;
+ }
+
+std::string System::OperatingSystem() { 
+    
+    return string();
+    
+ }
+
+int System::RunningProcesses() { 
+    return 0;
+    
+ }
+
+int System::TotalProcesses() { 
+    return 0;
+    
+ }
+
+long int System::UpTime() { 
+    
+    return 0;
+    
+ }
